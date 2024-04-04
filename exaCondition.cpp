7e8a659da78ae1f6ea3620129cefbff531617376
Smit Patel <114113998+Smit2208@users.noreply.github.com>
2024-04-04 19:21:46 -0400

    Update exaCondition.cpp

--- exaCondition.cpp
+++ exaCondition.cpp
@@ -33,5 +33,57 @@ int main()
         cout << "max is " << c << endl;
     }
 
+    if (a == b)
+    {
+        cout << "equal" << endl;
+    }
+    else
+    {
+        cout << "not equal " << endl;
+    }
+
+    /*while condition example*/
+        
+    int i, sum, x;
+    sum = 0;
+    i = 1;
+    x = 5;
+    
+    while (i <= 100)
+    {
+        sum = sum + x;
+        i = i +1;
+    }
+    cout << "sum is " << sum << endl;
+    /* taking values from user (from keyboard) */
+    int j, y, sum1 ;
+    j = 1;
+    y = 0;
+    sum1 =0;
+    
+    cout << "Enter the value " << endl;
+    cin >> y;
+    
+    while (j <= 100)
+    {
+        sum1 = sum1 + y;
+        j = j +1;
+    }
+    cout << "sum is " << sum1 << endl;
+
+    /*for loop and writing 2 x 1 = 2, 2 x 2 = 2 ....*/
+    
+    int sum, x;
+    sum = 0;
+    x = 5;
+    
+    for (int i = 1; i <= 10; i++)
+    {
+        int mul = 2 * i;
+        cout << "2 x " << i << " = " << mul << endl;
+    }
+
+    /*for loop with user input so I can write anyone 11eca(Gujarati) */
+
     return 0;
 }
