bd36d26d8772a3de40efe25726eefd90d7ba1ab2
Smit Patel <114113998+Smit2208@users.noreply.github.com>
2024-04-04 18:23:58 -0400

    Create exaCondition.cpp

---
+++ exaCondition.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    int b = 7;
+    
+    if (a <= b)
+    {
+        cout << "min is " << a << endl;
+    }
+    else
+    {
+        cout << "max is " << b << endl;
+    }
+
+    return 0;
+}
