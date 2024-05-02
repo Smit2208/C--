bccdb5b9446f36c266d701e988285059df0fb75e
Smit Patel <smitcomsci@gmail.com>
2024-05-02 15:42:19 -0400

    on final day

--- April18Lacture.cpp
+++ April18Lacture.cpp
@@ -3,8 +3,13 @@ using namespace std;
 
 int main()
 {
-    string text;
+    string text, text2;
     cout << "Enter the text: ";
     cin >> text;
+    //getline (cin, text);
+    cout << "your entered: " << text << endl;
+
+    getline(cin, text2);
+    cout << "you entered: " << text2 << endl;
     return 0;
 }
---
+++ practiceC.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number[] = {5,6,7,8,9}
+
+    for (int i = 0; i <= 5; i++)
+    {
+        cout << "Numbers: " << number[i] << endl;
+    }
+}
